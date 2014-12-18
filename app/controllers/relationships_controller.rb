6c067a91420a14f38517b458219c7a328fd8bdb8
Niger Hall <h.niger@yahoo.com>
2014-12-17 19:59:17 -0500

    redirected users and profiles after connect, rearranged connect adjusted relationships controller
    redirected users and profiles after connect, rearranged connect adjusted
    relationships controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,12 @@ class RelationshipsController < ApplicationController
     def create
         @user = User.find(params[:relationship][:watching_id])
         current_user.connect!(@user)
-        redirect_to @user
+        redirect_to @user.profile
     end
     
     def destroy
         @user = Relationship.find(params[:id]).watching
         current_user.remove!(@user)
-        redirect_to @user
+        redirect_to @user.profile
     end
 end
